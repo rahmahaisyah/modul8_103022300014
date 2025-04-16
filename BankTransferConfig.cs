12c2a2d403766e2fb6fa1c9e4fe7b259ba54acc5
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 17:22:58 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300014
+{
+    // Kelas ini merepresentasikan konfigurasi Transfer bank yang dapat dibaca dan disimpan ke file eksternal.
+    // Menerapkan konsep Runtime Configuration (nilai bisa berubah tanpa compile ulang).
+    internal class BankTransferConfig
+    {
+        public class Transfer
+        {
+
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+
+        public class Confirmation
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+        }
+
+        public class Config
+        {
+            // Properti konfigurasi yang akan disimpan/dibaca dari file JSON
+            public string lang { get; set; }
+            public Transfer transfer { get; set; }
+
+            public List<string> methods { get; set; }
+            public Confirmation confirmation { get; set; }
+        }
+
+
+        // Lokasi file konfigurasi
+        public const string filePath = @"bank_transfer_config.json";
+        //konstructor convig
+        public Config config;
+
+        //set semua velue defult
+        public void SetDefault() {
+            config = new Config();
+            config.lang = "en";
+            config.transfer.threshold = 25000000;
+            config.transfer.low_fee = 6500;
+            config.transfer.high_fee = 15000;
+            config.methods = ["“RTO", "(real-time)”", "“SKN”", "“RTGS”", "“BI", "FAST”"];
+            config.confirmation.en = "yes";
+            config.confirmation.id = "ya";
+        }
+
+        public Config ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            config = JsonSerializer.Deserialize<Config>(configJsonData);
+            return config;
+        }
+
+        public void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,87 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul8_103022300014;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat objek konfigurasi, membaca dari file JSON saat runtime(Runtime Configuration)
+        BankTransferConfig config = new BankTransferConfig();
+
+        // Menampilkan nilai konfigurasi setelah perubahan satuan
+        Console.WriteLine($"[DEBUG] Satuan suhu yang digunakan sekarang: {config.config.lang}");
+
+        //program dijalankan program akan menampilkan pesan sebagai berikut 
+        //tergantung nilai dari CONFIG1 atau JSON untuk key “lang”
+        if (config.config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+
+        } else
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+
+        // Input jumlah uang dari user
+        int jumlahTF = Convert.ToInt32(Console.ReadLine());
+
+        if(jumlahTF <= config.config.transfer.threshold)
+        {
+            Console.WriteLine($"transfer sejumlah {config.config.transfer.low_fee}");
+            if(config.config.lang == "en")
+            {
+                Console.WriteLine($"Transfer fee = {config.config.transfer.low_fee} and Total amount = {config.config.transfer.low_fee + jumlahTF}");
+            }
+            else
+            {
+                Console.WriteLine($"Biaya t ransfer = {config.config.transfer.low_fee} dan Total biaya = {config.config.transfer.low_fee + jumlahTF}");
+            }
+            
+            
+        }
+
+        if (config.config.lang == "en")
+        {
+            Console.WriteLine($"Select tranfer methode");
+        }
+        else
+        {
+            Console.WriteLine($"Pilih metode transfer");
+        }
+
+        foreach (var item in config.config.methods)
+        {
+            Console.WriteLine(item);
+
+        }
+
+        if (config.config.lang == "en")
+        {
+            Console.WriteLine($"Please type {config.config.confirmation.en} to confirm the transaction:");
+            string en = Console.ReadLine();
+            if (en == config.config.lang) {
+                Console.WriteLine("The transfer is completed");
+            }
+            else
+            {
+                Console.WriteLine("cancelled");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Ketik {config.config.confirmation.id} konfirmasi transaksi:");
+            string en = Console.ReadLine();
+            if (en == config.config.lang)
+            {
+                Console.WriteLine("berhasi;");
+            }
+            else
+            {
+                Console.WriteLine("batal");
+            }
+        }
+
+
+    }
+}
+
